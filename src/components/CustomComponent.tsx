83d7f0708da30ff7599357fd8038f22e8b06ca02
DiggesT <Parchomencodan@mail.ru>
2023-02-15 18:38:49 +0700

    Actions first steps

---
+++ src/components/CustomComponent.tsx
@@ -0,0 +1,7 @@
+import Axios, { AxiosRequestConfig, AxiosPromise, AxiosError } from 'axios' //test
+
+const CustomComponent = () => {
+  return <h2>This is custom component</h2>;
+};
+
+export default CustomComponent
