d57a4f308b712d82d0fb96f01459b0beb55e6631
DiggesT <Parchomencodan@mail.ru>
2023-02-16 15:48:22 +0700

    2nd try

--- src/components/CustomComponent.tsx
+++ src/components/CustomComponent.tsx
@@ -1,7 +1,9 @@
-import Axios, { AxiosRequestConfig, AxiosPromise, AxiosError } from 'axios' //test
+import Axios, { AxiosRequestConfig, AxiosPromise, AxiosError } from "axios"; //test
+
+import { postApiV1AuthLogin } from "../api/endpoints/auth";
 
 const CustomComponent = () => {
   return <h2>This is custom component</h2>;
 };
 
-export default CustomComponent
+export default CustomComponent;
