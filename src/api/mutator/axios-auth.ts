8963f37e49dec41a3aec259a2046f96a4bb5e6af
DiggesT <Parchomencodan@mail.ru>
2023-02-16 16:17:04 +0700

    4th try

--- src/api/mutator/axios-auth.ts
+++ src/api/mutator/axios-auth.ts
@@ -3,12 +3,16 @@ import Axios, { AxiosRequestConfig, AxiosPromise, AxiosError } from 'axios'
 //import { getAuthViaCookies, getRecord } from '../../auth'
 //import { LoggedIn as PLoggedIn } from '../endpoints/auth'
 
+import { LoggedIn as PLoggedIn } from '../endpoints/auth'
+
 export const AXIOS_INSTANCE = Axios.create({ baseURL: '/' })
 
 // add a second `options` argument here if you want to pass extra options to each generated query
 export const axiosAuth = <T>(config: AxiosRequestConfig, options?: AxiosRequestConfig): AxiosPromise<T> => {
   // const auth: string[][] = getAuthViaCookies()
   // const authHeaders = { headers: getRecord(auth) }
+
+  const Logged : PLoggedIn | undefined = undefined
 
   // const updatedOptions = options ? { ...options, authHeaders } : authHeaders
 
